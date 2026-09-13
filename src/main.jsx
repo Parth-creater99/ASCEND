31665b20d4ff606d9ef948afe1ecbf8a9f05beb8
Parth-creater99 <parthrajnish@gmail.com>
2026-09-13 10:24:14 +0530

    remove AuthWrapper and revert to localStorage to bypass database setup entirely

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AuthWrapper from './AuthWrapper.jsx'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthWrapper />
+    <App />
   </React.StrictMode>,
 )
